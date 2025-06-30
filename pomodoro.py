07b0a994c8ead316d8f8251d1de6e22d6af22d58
Kevin <kemazam@ists.edu.ec>
2025-06-30 08:50:50 -0500

    creando ventana prinicpal

--- pomodoro.py
+++ pomodoro.py
@@ -1,1 +1,19 @@
 # crear temporizador pomodoro con 25 min de trabajo, 5 min de descanso corto y 15 min de descanso largo
+
+import tkinter as tk 
+import time 
+import pygame 
+import threading
+from tkinter import ttk 
+from datetime import datetime, timedelta
+
+class temporizador_pomodoro:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Pomodoro")
+        self.root.geometry("500x500")
+        self.root.configure(bg='black')
+        self.root.resizable(False, False)
+
+    def run(self):
+        self.root.mainloop()
